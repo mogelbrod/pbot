fa98b5c95d71ec5f780534a56dde15220490473e
Victor Hallberg <victor@hallberg.cc>
2017-04-07 00:15:00 +0200

    Create backend abstraction & start experimenting with toplists

--- index.js
+++ index.js
@@ -1,105 +1,64 @@
 #!/usr/bin/env node
 // const promisify = require('js-promisify')
-const slackbots = require('slackbots')
-const Airtable = require('airtable')
-const config = require('./config')
-const air = new Airtable({apiKey: config.key}).base(config.base)
+const slackbots = require("slackbots")
+const backend = require("./lib/backend")
+const config = require("./config")
+const b = new backend.Backend(config)
 
-const TABLES = ['Members', 'Sessions', 'Drinks']
-const REQUIRED_TABLES = ['Sessions', 'Members']
-const LIST_ARGS = {
-  Sessions: {sort: [{field: 'Start'}]},
-  Members: {sort: [{field: 'Name'}]},
-  Drinks: {sort: [{field: 'Time'}]},
-}
-
-const state = exports.state = TABLES.reduce((s, table) => (s[table] = [], s), {})
 const commands = exports.commands = []
 
-function tableName(table) {
-  return table.replace(/^(.)(.+?)s?$/, (_, c, rest) => c.toUpperCase() + rest + 's')
-}
-
 function format(d) {
-  if (typeof d === "object" && d.fields) {
-    return JSON.stringify(d.fields, null, 2)
+  const t = typeof d
+  if (t === "object" && d.fields) {
+    const prefix = typeof d.getId === "function" ? d.getId() + " = " : ""
+    return prefix + JSON.stringify(d.fields, null, 2)
+  } else if (d != null) {
+    return d.toString()
   }
-  return d.toString()
 }
 
-function command(name, dependencies, fn) {
-  if (!fn) {
-    fn = dependencies
-    dependencies = REQUIRED_TABLES
-  }
-  fn.dependencies = dependencies
+function command(name, fn) {
   exports[name] = fn
   exports.commands.push(name)
   return fn
 }
 
-function list(table, args = null) {
-  table = tableName(table)
-  args = LIST_ARGS[table]
-  return new Promise((resolve, reject) => {
-    air(table).select(args).eachPage((items, next) => {
-      state[table] = items
-      console.log(`* Got ${items.length} rows from ${table}`)
-      resolve(items)
-    }, err => (err ? reject(err) : null))
-  })
-}
-
-function reload(...tables) {
-  return Promise.all(tables.map(t => list(t)))
-}
-
-function create(table, data) {
-  table = tableName(table)
-  return new Promise((resolve, reject) => {
-    air(table).create(data, (err, res) => {
-      if (err) {
-        err.inputData = data
-        return reject(err)
-      }
-      state[table].push(res)
-      resolve(res)
-    })
-  })
-}
-
 function session(index = "-1") {
   index = parseInt(index, 10)
-  if (index < 0) index += state.Sessions.length
-  const s = state.Sessions[index]
-  if (!s) {
-    throw new Error(`No sessions found`)
-  }
-  return s
+  return b.table("Sessions").then(sessions => {
+    if (index < 0) index += sessions.length
+    const s = sessions[index]
+    if (!s) {
+      throw new Error(`No sessions found`)
+    }
+    return s
+  })
 }
 
 function member(email) {
-  const m = state.Members.find(m => m.get('Email') === email)
-  if (!m) {
-    throw new Error(`No member with email '${email}'`)
-  }
-  return m
+  return b.table("Members").then(members => {
+    const m = members.find(m => m.get("Email") === email)
+    if (!m) {
+      throw new Error(`No member with email '${email}'`)
+    }
+    return m
+  })
 }
 
-command('session', ['Sessions'], session)
-command('member', ['Members'], member)
-command('list', [], list)
-command('reload', [], reload)
-for (let table of TABLES) {
-  command(table.toLowerCase(), [], () => list(table))
+command("session", session)
+command("member", member)
+command("list", (table) => b.table(table, null, false))
+command("reload", (...tables) => b.tables(...tables))
+for (let table of backend.TABLES) {
+  command(table.toLowerCase(), () => b.table(table))
 }
 
-command('start', (location = "Unknown") => create('Sessions', {
+command("start", (location = "Unknown") => b.create('Sessions', {
   Start: new Date().toISOString(),
   Location: location,
 }))
 
-command('drink', (memberEmail, volume = "40", type = "Beer") => create('Drinks', {
+command("drink", (memberEmail, volume = "40", type = "Beer") => b.create('Drinks', {
   Time: new Date().toISOString(),
   Session: [session().getId()],
   Member: [member(memberEmail).getId()],
@@ -107,19 +66,55 @@ command('drink', (memberEmail, volume = "40", type = "Beer") => create('Drinks',
   Type: type,
 }))
 
-command('help', [], () => {
-  return `* Available commands:\n` + exports.commands
+command("sum", (what = "-1") => {
+  const isSession = what.indexOf('@') < 0
+  return Promise.all([
+    isSession ? session(what) : member(what),
+    b.table('Members'),
+  ]).then(res => {
+    const parent = res[0]
+    const members = res[1]
+    return b.table("Drinks", {
+      filterByFormula: `${b.type(parent)} = '${b.normalizeDate(parent.get('Start'))}'`,
+    }).then(drinks => {
+      // TODO
+      const partitions = drinks.reduce((memo, drink) => {
+        const key = drink.fields[isSession ? 'Members' : 'Sessions'][0]
+        if (!memo[key]) {
+          memo[key] = []
+          memo[key].Value = 0
+          memo[key].Key = key
+          memo[key].Entity = members.find(member => member.getId() === key)
+          console.log(memo[key])
+        }
+        memo[key].push(drink)
+        memo[key].Value += drink.fields.Volume
+        return memo
+      }, {})
+
+      const ranked = Object.keys(partitions)
+        .map(key => partitions[key])
+        .sort((a, b) => b.Value - a.Value)
+
+      return ranked
+    })
+  })
+})
+
+command("help", () => {
+  const cmds = exports.commands
     .sort((a, b) => a.localeCompare(b))
     .map(cmd => {
       // Ugly and not at all guaranteed to work, but still fun :)
       const args = exports[cmd].toString()
-        .replace(/^function[^(]*/, '')
-        .replace(/(\s*=>\s*|{)[\s\S]+/, '')
-        .replace(/^\s*\(|\)\s*$/g, '')
+        .replace(/^function[^(]*/, "")
+        .replace(/(\s*=>\s*|{)[\s\S]+/, "")
+        .replace(/^\s*\(|\)\s*$/g, "")
         .split(/\s*,\*/)
-        .join(', ')
+        .join(", ")
       return `  ${cmd}(${args})`
     }).join("\n")
+  return `* Available commands:\n${cmds}`
 })
 
 exports.execute = function(args) {
@@ -147,14 +142,14 @@ exports.execute = function(args) {
     process.exit(1)
   }
 
-  return (reload(...command.dependencies))
+  return Promise.resolve()
     .then(() => command.apply(null, args))
     .then(result => {
       if (!Array.isArray(result)) result = [result]
       console.log(result.map(format).join("\n\n"))
     })
     .catch(err => {
-      console.error("* Error:\n" + err)
+      console.error("* Error:\n" + err.stack)
       if (err.inputData) console.error("* Input data was:\n" + JSON.stringify(err.inputData, null, 2))
     })
 }

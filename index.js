c7e4c250100f06a57905f66694f5c8670036b5ee
Victor Hallberg <victor@hallberg.cc>
2019-12-27 14:30:42 +0100

    Add --config=value flag support (currently config + fancy)

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 #!/usr/bin/env node
+const path = require("path")
 const {Backend} = require("./lib/backend")
 const commands = require("./lib/commands")
 const format = require("./lib/format")
-const config = require("./config")
 
 const args = process.argv.slice(2)
-const log = (...args) => console.log(format.log(...args))
 
+// Available options (with defaults)
+const options = {
+  config: "config.json",
+  fancy: true,
+}
+
+// Parse leading `--option(=value)` flags into `options` object
+let optionMatch
+while (args[0] && (optionMatch = args[0].match(/--(\w+)(?:=(.*))/))) {
+  let option = optionMatch[1]
+  let value = optionMatch[2]
+  switch (typeof options[option]) {
+    case "undefined":
+      throw new Error(`Unknown option '${option}'`)
+    case "boolean":
+      value = (value === "true" || value === "1")
+      break
+  }
+  options[option] = value
+  args.shift()
+}
+
+const config = require(path.resolve(options.config))
 const backend = new Backend(Object.assign({log}, config))
+
+function log(...args) {
+  console.log(format.log(...args))
+}
 
 // CLI mode
 if (require.main === module && args[0] !== "bot") {
-  format.basicOutput = ["1", "true"].indexOf(process.env.FANCY) < 0
+  format.setFancy(options.fancy)
 
   function output(res) {
     return console.log(format.fancy(res))

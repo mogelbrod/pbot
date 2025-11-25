431f7a08728e8b4a9a3c0c42002aceecf3fc7dcc
Victor Hallberg <victor@hallberg.cc>
2025-11-25 20:24:22 +0100

    fix(commands): Handle +- drink modifiers correctly for baserow

--- lib/commands.ts
+++ lib/commands.ts
@@ -250,7 +250,7 @@ command(
 command(
   'drink',
   'Registers a drink for a member',
-  function (volume = '40', type = 'Beer', member = '') {
+  async function (volume = '40', type = 'Beer', member = '') {
     if (!member && !this.user) {
       return rejectError(`A member must be provided`)
     }
@@ -263,56 +263,52 @@ command(
 
     const isModifier = volume[0] === '+' || volume[0] === '-'
 
-    return Promise.all([
+    const [session, memberRecord] = await Promise.all([
       this.backend.session(),
       this.backend.member(member || this.user),
-    ]).then((res) => {
-      const [session, member] = res
+    ])
 
-      // Simply create new drink record
-      if (!isModifier) {
-        return this.backend
-          .create('Drinks', {
-            Time: new Date().toISOString(),
-            Sessions: [session._id],
-            Members: [member._id],
-            Volume: intVolume,
-            Type: f.capitalize(type),
-          })
-          .then((drink) => [['Registered', drink, 'to', member]])
-      }
+    // Simply create new drink record
+    if (!isModifier) {
+      const drink = this.backend.create('Drinks', {
+        Time: new Date().toISOString(),
+        Sessions: [session._id],
+        Members: [memberRecord._id],
+        Volume: intVolume,
+        Type: f.capitalize(type),
+      })
+      return [['Registered', drink, 'to', memberRecord]]
+    }
 
-      // Modify last drink by member
-      return this.backend
-        .table('Drinks', {
-          filterByFormula: `${member._type} = '${member.Email}'`,
-          maxRecords: 1,
-          sort: [{ field: 'Time', direction: 'desc' }],
-        })
-        .then(([drink]) => {
-          if (!drink) {
-            throw new Error(
-              `${f.escape(member.Name)} doesn't appear to have any drinks to modify`,
-            )
-          }
+    const memberId =
+      this.config.backend === 'baserow' ? memberRecord._id : memberRecord.Email
 
-          const newVolume = parseInt(drink.Volume as any, 10) + intVolume
-          const deleted = newVolume <= 0
-
-          return this.backend[deleted ? 'delete' : 'update'](
-            'Drinks',
-            drink._id!,
-            deleted ? (undefined as any) : { Volume: newVolume },
-          ).then((updatedDrink) => [
-            [
-              deleted ? 'Deleted' : 'Updated',
-              deleted ? drink : updatedDrink,
-              `(${intVolume >= 0 ? '+' : ''}${intVolume}) belonging to`,
-              member,
-            ],
-          ])
-        })
+    const [drinkRecord] = await this.backend.table('Drinks', {
+      filterByFormula: `${memberRecord._type} = '${memberId}'`,
+      maxRecords: 1,
+      sort: [{ field: 'Time', direction: 'desc' }],
     })
+    if (!drinkRecord) {
+      throw new Error(
+        `${f.escape(memberRecord.Name)} doesn't appear to have any drinks to modify`,
+      )
+    }
+
+    const newVolume = parseInt(drinkRecord.Volume as any, 10) + intVolume
+    const deleted = newVolume <= 0
+    const updatedDrink = await this.backend[deleted ? 'delete' : 'update'](
+      'Drinks',
+      drinkRecord._id!,
+      deleted ? (undefined as any) : { Volume: newVolume },
+    )
+    return [
+      [
+        deleted ? 'Deleted' : 'Updated',
+        deleted ? drinkRecord : updatedDrink,
+        `(${intVolume >= 0 ? '+' : ''}${intVolume}) belonging to`,
+        memberRecord,
+      ],
+    ]
   },
 )
 
